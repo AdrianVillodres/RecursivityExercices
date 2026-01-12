ce41c4b79cd8c0dbd3efe40a1b382b07320fb1a1
AdrianVillodres <adrian.villodres.7e8@itb.cat>
2026-01-12 13:41:59 +0100

    feat: CountDigits method added

--- Program.cs
+++ Program.cs
@@ -4,13 +4,15 @@
     {
         public static void Main()
         {
-            CountDigits(1234);
+            Console.WriteLine(CountDigits(1234));
         }
 
 
         public static int CountDigits(int n)
         {
-            return 0;
+            if (n < 10) return 1;
+
+            return CountDigits( n / 10) + 1;
         }
 
     }
