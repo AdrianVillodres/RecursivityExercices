83feab614dea1a9a6a8aa74b57b2c17baffc30d3
AdrianVillodres <adrian.villodres.7e8@itb.cat>
2026-01-12 13:54:29 +0100

    feat: AddDigits method added, calculates the addition of the digits of a number

--- Program.cs
+++ Program.cs
@@ -4,19 +4,16 @@
     {
         public static void Main()
         {
-            Console.WriteLine(AddDigits(123));
+            Console.WriteLine(AddDigits(456));
         }
 
 
         public static int AddDigits(int n)
         {
-            //down
             if (n < 10) return n;
-            //down
 
-            //up
+            return AddDigits(n % 10) + AddDigits(n / 10);
 
-            //up
         }
 
 
