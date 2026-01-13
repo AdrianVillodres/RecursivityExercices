e23c166a3cea147b69ccd5401dd1938af0e6e018
AdrianVillodres <adrian.villodres.7e8@itb.cat>
2026-01-13 12:36:19 +0100

    Enhance CountDigits with explanatory comment
    Add clarification comment to CountDigits function

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@
 
         public static int CountDigits(int n)
         {
+            //the minimun number will be always 1 in the end, so the + 1 in the second return just adds 1 to the previous final number(wich is 1), then continues adding becuase the return accumulates the number
             if (n < 10) return 1;
 
             return CountDigits( n / 10) + 1;
